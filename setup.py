5c0ab84b77a05525db84c62146dcb7f8c28999f6
Jason Rebuck <jaschon@users.noreply.github.com>
2021-09-16 12:13:42 -0700

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name='im2a',
-    version='.3',
-    install_requires=open('requirements.txt').read().splitlines(),
-    packages=setuptools.find_packages()
-)
